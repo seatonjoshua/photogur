a23c4ff7835cebe0eab60308ba270cfdc4f31bd7
Joshua Seaton <joshuaseaton@Joshua-Seatons-MacBook-Pro.local>
2014-01-28 13:48:38 -0500

    Moving @pictures in pictures_controller.rb into database

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,22 +1,6 @@
 class PicturesController < ApplicationController
   def index
-    @pictures = [
-      {
-        :title  => "The old church on the coast of White sea",
-        :artist => "Sergey Ershov",
-        :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-      },
-      {
-        :title  => "Sea Power",
-        :artist => "Stephen Scullion",
-        :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-      },
-      {
-        :title  => "Into the Poppies",
-        :artist => "John Wilhelm",
-        :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-      }
-    ]
+    @pictures = Picture.all 
   end
 
   def picture0
@@ -43,25 +27,8 @@ class PicturesController < ApplicationController
     }
   end
 
-  def show
-    @pictures = [
-      {
-        :title  => "The old church on the coast of White sea",
-        :artist => "Sergey Ershov",
-        :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-      },
-      {
-        :title  => "Sea Power",
-        :artist => "Stephen Scullion",
-        :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-      },
-      {
-        :title  => "Into the Poppies",
-        :artist => "John Wilhelm",
-        :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-      }
-    ]
-    @picture = @pictures[params[:id].to_i]
+  def show 
+    @picture = Picture.find(params[:id])
   end
 
   def new
